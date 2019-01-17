b93ff90f8779c493541c80ece1b8835019b07914
VenkataReddyBonam <46799681+VenkataReddyBonam@users.noreply.github.com>
2019-01-18 10:25:28 +1100

    Add files via upload

---
+++ FootBallWinForms/FootBallWinForms/FootBall.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace FootBallWinForms
+{
+    partial class FootBall
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDataFile = new System.Windows.Forms.TextBox();
+            this.lblDataFile = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtDataFile
+            // 
+            this.txtDataFile.Location = new System.Drawing.Point(132, 26);
+            this.txtDataFile.Name = "txtDataFile";
+            this.txtDataFile.ReadOnly = true;
+            this.txtDataFile.Size = new System.Drawing.Size(446, 20);
+            this.txtDataFile.TabIndex = 0;
+            // 
+            // lblDataFile
+            // 
+            this.lblDataFile.AutoSize = true;
+            this.lblDataFile.Location = new System.Drawing.Point(42, 29);
+            this.lblDataFile.Name = "lblDataFile";
+            this.lblDataFile.Size = new System.Drawing.Size(83, 13);
+            this.lblDataFile.TabIndex = 2;
+            this.lblDataFile.Text = "Choose data file";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.DefaultExt = "csv";
+            this.openFileDialog.Filter = "csv files|*.csv";
+            this.openFileDialog.InitialDirectory = "c:\\";
+            this.openFileDialog.ReadOnlyChecked = true;
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(584, 24);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(75, 23);
+            this.btnUpload.TabIndex = 3;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(45, 80);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 5;
+            this.lblResult.Visible = false;
+            // 
+            // FootBall
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.lblDataFile);
+            this.Controls.Add(this.txtDataFile);
+            this.Name = "FootBall";
+            this.Text = "Foot Ball";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDataFile;
+        private System.Windows.Forms.Label lblDataFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ FootBallWinForms/FootBallWinForms/FootBall.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FootBallWinForms
+{
+    public partial class FootBall : Form
+    {
+        public FootBall()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            //I don't have much knowledge about Football game. So making the following assumptions on the requirements
+
+            //Assumption 01: The csv data file supplied has hiphen '-' as the column header for the 7th Column. 
+            //Assumming this is the format expected.
+
+            //Assumption 02: It's been mentioned in the instructions document to validate only the columns. 
+            //So assuming just to validate the header row for the column headings and 
+            //row data is not required to be validated and ignoring the invalid row data. 
+            // The data file supplied has just dashes '-' as the 19th row data and ignoring this row.
+
+            //Assumption 03: The requirement is to find the team name which has the smallest difference between Columns 'F' and 'A' data
+            //which means the smallest absolute difference.
+            
+            try
+            {
+                //clearing the results so that multiple executions of the functionality will be showing the correct results.
+                lblResult.Visible = false;
+                lblResult.Text = string.Empty;
+
+                //showing the file dialog to choose the file
+                DialogResult result = openFileDialog.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    txtDataFile.Text = openFileDialog.FileName;
+
+                    string strFilePath = openFileDialog.FileName;
+
+                    string[] strFilePathSplit = strFilePath.Split('\\');
+
+                    string strFileName = @"Data\" + DateTime.Now.ToString("yyyyMMddHHmmss") + "_" + strFilePathSplit[strFilePathSplit.Length - 1];
+
+                    //saving the file to the solution to have the history of the files.
+                    //This step may not be required for this case. But in real applications we may want to keep the history
+                    File.Copy(strFilePath, strFileName);
+
+                    //Reading the csv file data
+                    string csvData = File.ReadAllText(txtDataFile.Text);
+
+                    DataTable dtCsvData = new DataTable();
+
+                    dtCsvData.Columns.AddRange(new DataColumn[9] { new DataColumn("Team", typeof(string)),
+                            new DataColumn("P", typeof(int)),
+                            new DataColumn("W",typeof(int)),
+                            new DataColumn("L",typeof(int)),
+                            new DataColumn("D", typeof(int)),
+                            new DataColumn("F",typeof(int)),
+                            new DataColumn("-",typeof(string)),
+                            new DataColumn("A", typeof(int)),
+                            new DataColumn("Pts",typeof(int))
+                    });
+
+                    for (int i = 0; i < csvData.Split('\n').Length; i++)
+                    {
+                        string row = csvData.Split('\n')[i];
+                        //assuming the first row should be the header row
+                        //validating the columns names of the header row
+                        if (i == 0 && !string.IsNullOrEmpty(row))//header row
+                        {
+                            string[] strHeaderRowSplit = row.Split(',');
+                            if (strHeaderRowSplit.Length != 9)
+                            {
+                                MessageBox.Show("Invalid Header Row. The total number of expected columns is 9.", "FootBall", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                return;
+                            }
+                            for (int j = 0; j < strHeaderRowSplit.Length; j++)
+                            {
+                                if (strHeaderRowSplit[j] != dtCsvData.Columns[j].ColumnName)
+                                {
+                                    MessageBox.Show(String.Format("Invalid Header Row.The column name in csv file at position {0} should be {1}", j + 1, dtCsvData.Columns[j].ColumnName), "FootBall", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                    return;
+                                }
+                            }
+                            continue;//header row is valid. So continue to read the rows data.
+                        }
+                        if (!string.IsNullOrEmpty(row))
+                        {
+                            if (row.Split(',').Length != 9)
+                                continue;//invalid row data. So as per the assumption skipping the invalid row
+
+                            dtCsvData.Rows.Add();
+                            int j = 0;
+
+                            foreach (string cell in row.Split(','))
+                            {
+                                dtCsvData.Rows[dtCsvData.Rows.Count - 1][j] = cell.Trim();
+                                j++;
+                            }
+                        }
+                    }
+                    //ordering the data table data by abosulte difference of Column's 'F' and 'A' and getting the smallest valued row.
+                    var dataRow = dtCsvData.AsEnumerable().OrderBy(r => Math.Abs((r.Field<int>("F") - r.Field<int>("A")))).FirstOrDefault();
+                    if (dataRow != null)
+                    {
+                        lblResult.Text = "The team with the smallest difference in ‘for’ and ‘against’ goals is : " + dataRow.Field<string>("Team");
+                        lblResult.Visible = true;
+                    }
+
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("The following error occurred while processing the request. "+ex.Message, "FootBall", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
